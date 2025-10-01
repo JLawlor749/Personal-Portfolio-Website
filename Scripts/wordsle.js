5fc5577fb8261fc89affda2eed69b5e452c97d7b
JAMES LAWLOR <jamie.lawlor2003@gmail.com>
2025-10-01 15:49:05 +0100

    Added new images for Wordsle and SegFault to replace placeholders. Added scripts folder to contain JavaScript files. Added new Wordsle page which will run a version of the Wordsle game in JS.

---
+++ Scripts/wordsle.js
@@ -0,0 +1,15 @@
+function check_word(word, target_word, word_length)
+{
+    /*
+    This function checks each letter in the guessed word against the target.
+    It prints each letter of the guess in a different colour based on correctness.
+    GREEN = Correct letter in the correct space.
+    YELLOW = Correct letter in the wrong space.
+    RED = Wrong letter.
+    */
+
+    goal_checklist = []
+    guess_print = []
+
+    
+}
